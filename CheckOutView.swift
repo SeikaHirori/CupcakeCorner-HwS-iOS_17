463330396874de91a3b7e3f9b0097d8252cc2188
SeikaHirori <SeikaHirori@outlook.com>
2024-01-16 15:36:52 -0800

    Started section: "Checking for a valid address"
    https://arc.net/l/quote/xmwdqlab

---
+++ CheckOutView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckOutView.swift
+//  CupcakeCorner-HwS-iOS_17
+//
+//  Created by Seika Hirori on 1/16/24.
+//
+// Section: "Checking for a valid address"
+// https://arc.net/l/quote/xmwdqlab
+
+
+import SwiftUI
+
+struct CheckOutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckOutView(order: Order())
+}
