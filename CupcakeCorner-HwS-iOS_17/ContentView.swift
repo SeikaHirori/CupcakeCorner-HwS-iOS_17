0c082922120e53058766e15f080675d5f5d5fb95
SeikaHirori <SeikaHirori@outlook.com>
2024-01-16 12:26:47 -0800

    Initial Commit

---
+++ CupcakeCorner-HwS-iOS_17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner-HwS-iOS_17
+//
+//  Created by Seika Hirori on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner-HwS-iOS_17/CupcakeCorner_HwS_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_HwS_iOS_17App.swift
+//  CupcakeCorner-HwS-iOS_17
+//
+//  Created by Seika Hirori on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_HwS_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
