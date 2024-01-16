84fd4f2c0ea49816fff603b5bb4ad7bc822acceb
SeikaHirori <SeikaHirori@outlook.com>
2024-01-16 14:07:40 -0800

    Finished section "Loading an image from a remote server"
    https://www.hackingwithswift.com/books/ios-swiftui/loading-an-image-from-a-remote-server

--- CupcakeCorner-HwS-iOS_17/ContentView.swift
+++ CupcakeCorner-HwS-iOS_17/ContentView.swift
@@ -13,13 +13,17 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-                
-                image
-                    .resizable()
-                    .scaledToFill()
-            } placeholder: {
-                Color.red
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                }
+                else if phase.error != nil {
+                    Text("There was an error loading the image.")
+                } else {
+                    ProgressView()
+                }
             }
             .frame(width: 200, height: 200)
         }
