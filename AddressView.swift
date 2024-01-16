4bc91d5e96826ea82b0b708af47cacc73555ab2d
SeikaHirori <SeikaHirori@outlook.com>
2024-01-16 15:32:27 -0800

    Finished section "Taking basic order details"
    https://arc.net/l/quote/hzynmpsf

---
+++ AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner-HwS-iOS_17
+//
+//  Created by Seika Hirori on 1/16/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
